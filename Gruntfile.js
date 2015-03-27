5d7b6f5a4aac1b401f9783116ec34c4ed930a7ad
pepelsbey <pepelsbey@gmail.com>
2015-03-27 15:27:19 +0100

    Better package.json-based banner

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,7 +26,7 @@ module.exports = function(grunt) {
 		usebanner: {
 			task: {
 				options: {
-					banner: '/**\n * <%= pkg.description %>\n * <%= pkg.name %> v<%= pkg.version %>, <%= pkg.homepage %>\n * Copyright © 2010–<%= grunt.template.today("yyyy") %> Vadim Makeev, http://pepelsbey.net\n * Licensed under MIT license: <%= pkg.licenses[0].url %>\n */'
+					banner: '/**\n * <%= pkg.description %>\n * <%= pkg.name %> v<%= pkg.version %>, <%= pkg.homepage %>\n * @copyright 2010–<%= grunt.template.today("yyyy") %> <%= pkg.author.name %>, <%= pkg.author.url %>\n * @license <%= pkg.licenses[0].type %>, <%= pkg.licenses[0].url %>\n */'
 				},
 				files: {
 					'styles/screen.css': 'styles/screen.css'
