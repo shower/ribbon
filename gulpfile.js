6dc4170e8cd505197060d2165257683f4529030f
Vadim Makeev <pepelsbey@gmail.com>
2015-07-31 00:11:01 +0200

    Another fresh start

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
-var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')({
-	rename: {
-		'gulp-concat-util': 'concat'
-	}
-});
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	cssnano = require('cssnano');
+
 var pkg = require('./package.json');
 var banner = [
 	'/**',
@@ -14,16 +13,39 @@ var banner = [
 	' */\n'
 ].join('\n');
 
-function styles(ratio) {
-	return gulp.src('styles/screen.scss')
-		.pipe(plugins.rename('screen--' + ratio + '.scss'))
-		.pipe(plugins.concat_util.header('$ratio:' + ratio + ';'))
-		.pipe(plugins.sass())
-		.pipe(plugins.autoprefixer())
-		.pipe(plugins.cssmin())
-		.pipe(plugins.concat.header(banner))
+gulp.task('default', function () {
+	return gulp.src('styles/screen--*.scss')
+		.pipe(sass())
+		.pipe(autoprefixer())
 		.pipe(gulp.dest('styles/'));
-}
+});
+
+// var gulp = require('gulp');
+// var plugins = require('gulp-load-plugins')({
+// 	rename: {
+// 		'gulp-concat-util': 'concat'
+// 	}
+// });
+// var pkg = require('./package.json');
+// var banner = [
+// 	'/**',
+// 	' * <%= pkg.description %>',
+// 	' * <%= pkg.name %> v<%= pkg.version %>, <%= pkg.homepage %>',
+// 	' * @copyright 2010–<%= new Date().getFullYear() %> <%= pkg.author.name %>, <%= pkg.author.url %>',
+// 	' * @license <%= pkg.licenses[0].type %>, <%= pkg.licenses[0].url %>',
+// 	' */\n'
+// ].join('\n');
+
+// function styles(ratio) {
+// 	return gulp.src('styles/screen.scss')
+// 		.pipe(plugins.rename('screen--' + ratio + '.scss'))
+// 		.pipe(plugins.concat_util.header('$ratio:' + ratio + ';'))
+// 		.pipe(plugins.sass())
+// 		.pipe(plugins.autoprefixer())
+// 		.pipe(plugins.cssmin())
+// 		.pipe(plugins.concat.header(banner))
+// 		.pipe(gulp.dest('styles/'));
+// }
 
 // function styles(ratio) {
 // 	return gulp.src([
@@ -38,10 +60,10 @@ function styles(ratio) {
 // 	// 	.pipe(gulp.dest('styles/'));
 // }
 
-gulp.task('16x10', function() { return styles('16/10'); });
-gulp.task('4x3', function() { return styles('4/3'); });
+// gulp.task('16x10', function() { return styles('16/10'); });
+// gulp.task('4x3', function() { return styles('4/3'); });
 
-gulp.task('default', ['16x10', '4x3']);
+// gulp.task('default', ['16x10', '4x3']);
 
 // function bump(importance) {
 // 	return gulp.src(['./package.json', './bower.json'])
